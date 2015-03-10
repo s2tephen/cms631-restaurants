38d8b4f8ef3ac388d153cd3a77377c32180ef54f
Stephen Suen <ssuen@mit.edu>
2015-03-10 15:04:03 -0400

    Renamed to data.

--- concat.sh
+++ concat.sh
@@ -1,4 +1,4 @@
-mkdir -p clean
+mkdir -p data
 files=$(find CMS.631-s15-Favorite-Restaurants -name "*.csv" -exec echo -n "{} " \;)
 for f in $files; do sed 's/\", /\",/g' $f | csvformat -U 1 > data/${f:33}; done
 csvstack -u 1 $(find data -type f -exec echo -n "{} " \;) > restaurants.csv
